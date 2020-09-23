ec169b32b8c9b03eb9abab23e6e03bc06596cda8
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-09-23 11:41:01 +0200

    Updates

---
+++ precache-manifest.66bd13eea0708d6cedadc3f7e568baad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f56aa15e1cecbdc81eecd0b93d4668",
+    "url": "/nasa-app/index.html"
+  },
+  {
+    "revision": "b6790a9fb5e817c97f26",
+    "url": "/nasa-app/static/css/main.d4e1fc64.chunk.css"
+  },
+  {
+    "revision": "f47e71e3bb8fea736ca7",
+    "url": "/nasa-app/static/js/2.00cc48a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-app/static/js/2.00cc48a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6790a9fb5e817c97f26",
+    "url": "/nasa-app/static/js/main.8a75d6d3.chunk.js"
+  },
+  {
+    "revision": "c3970e2f9a98ce78f24e",
+    "url": "/nasa-app/static/js/runtime-main.1c4d3ad0.js"
+  },
+  {
+    "revision": "322e6f116caee846f9ed4431f3cd925d",
+    "url": "/nasa-app/static/media/Logo.322e6f11.svg"
+  },
+  {
+    "revision": "0debd2e06a964b6c175ac71af680cc7c",
+    "url": "/nasa-app/static/media/astronaut.0debd2e0.svg"
+  },
+  {
+    "revision": "0a616663c0b7a1b7f61943bc84f2c3f1",
+    "url": "/nasa-app/static/media/rocket_launch_.0a616663.svg"
+  }
+]);
